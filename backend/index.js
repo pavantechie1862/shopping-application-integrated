1c86049edb1b9e12bfa4d7d79bbd6a36088ad5e5
Pavan Marapalli <pavanmarapalli171862@gmail.com>
2023-05-30 18:48:03 +0530

    30th may

--- backend/index.js
+++ backend/index.js
@@ -40,20 +40,15 @@ initializeDBandServer();
 const authenticateToken = (request, response, next) => {
   const authHeaders = request.headers;
   if (authHeaders === undefined) {
-    console.log("jwt is undefined");
     response.send("Unauthorised  from middleware jwt is undefined");
     response.status(401);
   } else {
-    console.log(authHeaders);
     const jwtToken = authHeaders["authorization"].split(" ")[1];
     jwt.verify(jwtToken, "MY_SECRET_TOKEN", (error, payload) => {
       if (error) {
-        console.log("jwt not matched");
         response.status(401);
-
         response.send("UnAuthorised");
       } else {
-        console.log("in authenticate token");
         next();
       }
     });
@@ -62,8 +57,6 @@ const authenticateToken = (request, response, next) => {
 
 app.post("/register", async (request, response) => {
   const userData = request.body;
-  console.log(userData);
-  console.log(request.body);
   const { username, name, password } = userData;
 
   const checkUserQuery = `
@@ -124,44 +117,124 @@ app.post("/login/", async (request, response) => {
 });
 
 app.get("/products/", authenticateToken, async (request, response) => {
-  console.log("in api");
   const {
-    sort_by = "PRICE_LOW",
+    sort_by = "",
     category = "",
     title_search = "",
     rating = 1,
   } = request.query;
-
-  const order = sort_by === "PRICE_LOW" ? "ASC" : "DESC";
-
   const getProductsQuery = `
-  SELECT * FROM products WHERE 
-  title like '%${title_search}%' 
-  and rating >= ${rating} 
-  and category like '%${category}%'
-  ORDER BY price ${order}
+        SELECT
+        * 
+        FROM 
+        products 
+        where 
+        title like '%${title_search}%' and 
+        category like '%${category}%' and 
+        rating >= ${rating} 
+        order by price ${sort_by}
   `;
-
   const productsList = await db.all(getProductsQuery);
   response.status(200);
   response.send(productsList);
 });
 
-app.post("/admin/addproduct/", async (request, response) => {
-  const product = request.body;
-  const { title, brand, price, id, image_url, rating, category } = product;
+app.get(
+  "/products/:productId/",
+  authenticateToken,
+  async (request, response) => {
+    const { productId } = request.params;
+    const getProductQuery = `
+  select * from products where id = ${productId}
+  `;
+    const productDetails = await db.get(getProductQuery);
+    response.send(productDetails);
+    response.status(200);
+  }
+);
 
-  const addProductQuery = `
-  INSERT INTO  products(title,brand,price,id,image_url,rating,category)
-  VALUES 
-  (
-    '${title}','${brand}','${price}','${id}','${image_url}','${rating}','${category}'
-  )
+app.get(
+  "/similar-products/:id",
+  authenticateToken,
+  async (request, response) => {
+    const { id } = request.params;
+    const getCatrgoryQuery = `
+  SELECT 
+    category
+  FROM
+    products
+  WHERE 
+    category like (
+      SELECT 
+        category 
+      FROM 
+        products 
+      WHERE 
+        id = ${id})
+    `;
+
+    const dbResponse = await db.get(getCatrgoryQuery);
+
+    const getSimilarCategoryProducts = `
+  SELECT 
+      *
+  FROM
+      products 
+  WHERE
+      category= '${dbResponse.category}' and id <> ${id}
   `;
 
-  const dbResponse = await db.run(addProductQuery);
-  response.status(200);
-  response.send({ lastID: dbResponse.lastID });
+    const similarProducts = await db.all(getSimilarCategoryProducts);
+    response.send(similarProducts);
+  }
+);
+
+app.get("/other-products/:id", authenticateToken, async (request, response) => {
+  const { id } = request.params;
+  const getCatrgoryQuery = `
+  SELECT 
+    category
+  FROM
+    products
+  WHERE 
+    category like (
+      SELECT 
+        category 
+      FROM 
+        products 
+      WHERE 
+        id = ${id})
+    `;
+
+  const dbResponse = await db.get(getCatrgoryQuery);
+  const otherProductsQuery = `
+    SELECT 
+        *
+    FROM
+        products 
+    WHERE
+        category <> '${dbResponse.category}'
+    `;
+
+  const otherProducts = await db.all(otherProductsQuery);
+  response.send(otherProducts);
+});
+
+app.get("/prime-deals/", authenticateToken, async (request, response) => {});
+
+app.post("/admin/addproduct/", async (request, response) => {
+  const product = request.body;
+  console.log("entered into api");
+  const { title, brand, price, id, image_url, rating, category } = product;
+  for (let each of jsonData) {
+    const { id, totalReviews, availability, description } = each;
+    console.log("before update statement");
+    const updateQuery = `
+  update products set total_reviews = '${totalReviews}',availability = '${availability}',description = '${description}' where id = '${id}';
+  `;
+
+    await db.run(updateQuery);
+  }
 });
 
 module.exports = app;
